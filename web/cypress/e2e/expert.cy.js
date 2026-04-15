615183d5fcf1b041f4ee7c65ad805e5f81abad20
João Ishida <joaovittorishida@gmail.com>
2026-04-14 22:32:38 -0300

    Novos testes com ferramentas e validações especificas do Cypress

--- web/cypress/e2e/expert.cy.js
+++ web/cypress/e2e/expert.cy.js
@@ -20,5 +20,50 @@ describe('Expert', () => {
 
     })
 
+    it('Não deve logar, senha inválida', () => {
+        cy.submitLoginForm('papito@webdojo.com', 'senhaerrada')
 
+        cy.get('[data-sonner-toaster=true]')
+            .should('be.visible')
+            .as('toast')
+
+        cy.get('@toast')
+            .find('.title')
+            .should('have.text', 'Acesso negado! Tente novamente.')
+
+        cy.wait(5000)
+
+        cy.get('@toast')
+            .should('not.exist')
+    })
+
+    it('Simulando tecla TAB', () => {
+
+        cy.get('body').press('Tab')
+        cy.focused().should('have.attr', 'id', 'email')
+
+        cy.get('#email').press('Tab')
+        cy.focused().should('have.attr', 'id', 'password')
+
+    })
+
+    it('Pressionar tecla Enter', () => {
+
+        cy.get('#email').type('papito@webdojo.com')
+        cy.get('#password').type('senhaerrada{Enter}')
+        
+
+        cy.get('[data-sonner-toaster=true]')
+            .should('be.visible')
+            .as('toast')
+
+        cy.get('@toast')
+            .find('.title')
+            .should('have.text', 'Acesso negado! Tente novamente.')
+
+        cy.wait(5000)
+
+        cy.get('@toast')
+            .should('not.exist')
+    })
 })
